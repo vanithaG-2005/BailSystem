e9d13aa670fa0c25106ceda8c716ea7df64af76d
vanithaG-2005 <vaninavi2005@gmail.com>
2025-08-13 12:04:14 +0530

    Update Register.js

--- Frontend/src/components/Register.js
+++ Frontend/src/components/Register.js
@@ -24,7 +24,7 @@ function Register() {
       return;
     }
     try {
-      await axios.post('http://localhost:5000/api/auth/register', {
+      await axios.post('https://bailsystem.onrender.com/api/auth/register', {
         username,
         password,
         role,
@@ -79,5 +79,6 @@ function Register() {
     </div>
   );
 }
+
 
 export default Register;
